77f7d923288cb15816f547535caa5699456c0de8
DanOliverRunoLarsen <42993978+DanOliverRunoLarsen@users.noreply.github.com>
2018-10-25 13:34:16 +0200

    Plant vs Zombie game basic af

--- src/Game.java
+++ src/Game.java
@@ -6,6 +6,8 @@ public class Game {
             while (zombie1.hp != 0 && plant1.hp != 0) {
                 zombie1.zombieAttack();
                 plant1.plantTakeDmg(zombie1.dmg);
+                if (plant1.hp == 0)
+                break;
                 plant1.plantAttack();
                 zombie1.zombieTakeDmg(plant1.dmg);
             }
@@ -16,11 +18,6 @@ public class Game {
             {
                 System.out.println("PLANT WINS");
             }
-else
-            {
-                System.out.println("HUGE ERROR 404");
-
-        }
 
 
     }
