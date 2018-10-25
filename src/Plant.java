b9e4402626211a6c323623f642c4b9534ca814a6
DanOliverRunoLarsen <42993978+DanOliverRunoLarsen@users.noreply.github.com>
2018-10-25 12:56:41 +0200

    start til PvZ

---
+++ src/Plant.java
@@ -0,0 +1,9 @@
+public class Plant {
+    int hpPlant = 100;
+    int dmgPlant = 10;
+
+    public Plant(int hpPlant, int dmgPlant) {
+        this.hpPlant = hpPlant;
+        this.dmgPlant = dmgPlant;
+    }
+}
---
+++ src/Zombie.java
@@ -0,0 +1,11 @@
+public class Zombie {
+    int hpZombie = 100;
+    int dmgZombie = 10;
+
+    public Zombie(int hpZombie, int dmgZombie) {
+        this.hpZombie = hpZombie;
+        this.dmgZombie = dmgZombie;
+    }
+
+    
+}
